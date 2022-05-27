514109f5fc93cc388e8e38267381f3051e67b130
horleon024 <horeftosleon@gmail.com>
2022-05-27 11:29:14 +0200

    add eventlisteners

--- script.js
+++ script.js
@@ -8,15 +8,44 @@ const gameBoard = (() => {
 	let _gameArray = new Array(9);
 
 	const clearBoard = () => {
-		for (let i = 0; i < 9; i++) gameArray[i] = "";
+		for (let i = 0; i < 9; i++) _gameArray[i] = "";
 	};
 
-	const setField = () => {
-
+	const setField = (index, sign) => {
+		_gameArray[index] = sign;
 	};
+
+	const getField = (index) => {
+		return _gameArray[index];
+	}
 
 	return {
 		clearBoard,
-		setField
+		setField,
+		getField
 	};
+  })();
+
+  const displayController = (() => {
+	const _restartButton = document.querySelector(".restart-button");
+	const _gameState = document.querySelector(".game-state");
+	const _gameFields = document.querySelectorAll(".game-field");
+
+	const _updateGameField = (event) => {
+
+	};
+
+	_gameFields.forEach((gameField) => gameField.addEventListener("click", _updateGameField));
+
+	const _restartGame = () => {
+		gameBoard.clearBoard();
+
+	};
+
+	_restartButton.addEventListener("click", _restartGame);
+
+	const setGameState = (state) => {
+		_gameState.textContent = state;
+	};
+	return { setGameState };
   })();
